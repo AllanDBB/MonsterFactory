f7e6022cc2c24bfa0c0233b61d80f8412e058901
natiorozco <128880489+natiorozco@users.noreply.github.com>
2024-09-30 23:18:33 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,89 @@
 #include <bits/stdc++.h>
 // Files:
 
-#include "./source/sources.h"
-#include "./source/combiner.h"
-#include "./source/furnace.h"
+#include "sources.h"
+#include "combiner.h"
+#include "delivery.h"
+#include "furnace.h"
+#include "quality.h"
+
+string getTraits(string s) {
+    if (s == "INTELIGENCIA") {
+        return "Cósmica, Orgánico, Astuta";
+    } else if (s == "DESTRUCCION") {
+        return "Elemental, Radioactivo, Caótica";
+    } else if (s == "REGENERACION") {
+        return "Oscura, Orgánico, Despiadada";
+    } else if (s == "FUERZA") {
+        return "Elemental, Metálico, Despiadada";
+    } else if (s == "MALDAD") {
+        return "Oscura, Metálico, Astuta";
+    } else if (s == "VENENO") {
+        return "Oscura, Radioactivo, Despiadada";
+    } else if (s == "LOCURA") {
+        return "Cósmica, Radioactivo, Caótica";
+    } else if (s == "TECNOLOGIA") {
+        return "Cósmica, Metálico, Astuta";
+    } else if (s == "VELOCIDAD") {
+        return "Elemental, Orgánico, Caótica";
+    } else {
+        return "Unknown trait";
+    }
+}
+
+
+namespace fs = filesystem;
+
+int m=1;
+int n=1;
+void leerArchivos(Delivery * pedidos) {
+    string carpeta = "C:/Users/natal/Desktop/u/2024/segundo semestre/estructuras de datos/pedidos";
+    for (const auto& entrada : fs::directory_iterator(carpeta)) {
+        if (entrada.is_regular_file()) {
+            string rutaArchivo = entrada.path().string();
+            ifstream archivo(rutaArchivo);
+            string linea;
+
+            if (archivo.is_open()) {
+                cout << "Leyendo archivo: " << rutaArchivo << endl;
+                string nombre="";
+                if (getline(archivo, linea)) {
+                    nombre = linea;
+                }
+                bool prioridad=false;
+                if (getline(archivo, linea)) {
+                    if (linea == "1st PROGRA") {
+                        prioridad = true;
+                    }
+                }
+
+                Client * cliente1 = new Client(nombre,prioridad,n);
+
+                n++;
+
+                while (getline(archivo, linea)) {
+
+                    if (linea == "") {
+                        break;
+                    }
+
+                    Monster * nuevo = new Monster(linea,getTraits(linea),prioridad);
+
+                    m++;
+                    cliente1->insert(nuevo);
+                };
+
+                pedidos->insert(cliente1);
+                archivo.close();
+            } else {
+                cerr << "No se pudo abrir el archivo: " << rutaArchivo << endl;
+            }
+
+            cout << "---------------------" << endl;
+        }
+    }
+}
+
 
 int main() {
     srand(time(0));
@@ -53,6 +133,7 @@ int main() {
     Combiner combiner(materialQueue, energyQueue, evilQueue, queueToBaking, monsterGarbage);
     //Monster* temp = combiner.combine();
     //cout << temp->type;
+
     MonsterQueue* toQuality = new MonsterQueue();
     Furnace furnace(10, 1, 1, 1,1, toQuality);
     int i = 0;
@@ -67,6 +148,7 @@ int main() {
             i++;
 
             furnace.add(queueToBaking->dequeue());
+
         }
     }
 
@@ -76,6 +158,20 @@ int main() {
     furnace.playTray(4);
 
     furnace.empty();
+
+
+    Storage * storage = new Storage();
+    GarbageCollector * garbage = new GarbageCollector();
+    Quality * quality = new Quality(storage, garbage, toQuality);
+
+    quality->activate();
+
+    Delivery * delivery = new Delivery(storage);
+    leerArchivos(delivery);
+    delivery->activate();
+
+
+
 
 
 
