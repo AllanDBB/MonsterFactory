65e8a6bbdad771b6bb2e9cbb576ea87760603cd8
natiorozco <128880489+natiorozco@users.noreply.github.com>
2024-09-30 23:16:33 -0600

    Update quality.h

--- source/quality.h
+++ source/quality.h
@@ -6,9 +6,80 @@
 #include <bits/stdc++.h>
 #include "monster.h"
 #include "inspector.h"
+#include "garbageCollector.h"
 
 #ifndef QUALITY_H
 #define QUALITY_H
+
+string getTimestamp() {
+    auto ahora = chrono::system_clock::now();
+    time_t tiempoActual = chrono::system_clock::to_time_t(ahora);
+    tm* fechaHora = localtime(&tiempoActual);
+    stringstream ss;
+    ss << put_time(fechaHora, "%Y-%m-%d %H:%M:%S");
+    return ss.str();
+}
+void writeToStorageLog(const string& mensaje) {
+    ofstream bitacora;
+
+    bitacora.open("C:/Users/natal/Desktop/u/2024/segundo semestre/estructuras de datos/bitacoraAlmacen.txt", ios::app);
+
+    if (bitacora.is_open()) {
+        bitacora << mensaje << endl;
+        bitacora.close();
+    } else {
+        cout << "No se pudo abrir el archivo de bitácora." << endl;
+    }
+}
+void writeToInspector1Log(const string& mensaje) {
+    ofstream bitacora;
+
+    bitacora.open("C:/Users/natal/Desktop/u/2024/segundo semestre/estructuras de datos/bitacoraInspector1.txt", ios::app);
+
+    if (bitacora.is_open()) {
+        bitacora << mensaje << endl;
+        bitacora.close();
+    } else {
+        cout << "No se pudo abrir el archivo de bitácora." << endl;
+    }
+}
+void writeToInspector2Log(const string& mensaje) {
+    ofstream bitacora;
+
+    bitacora.open("C:/Users/natal/Desktop/u/2024/segundo semestre/estructuras de datos/bitacoraInspector2.txt", ios::app);
+
+    if (bitacora.is_open()) {
+        bitacora << mensaje << endl;
+        bitacora.close();
+    } else {
+        cout << "No se pudo abrir el archivo de bitácora." << endl;
+    }
+}
+void writeToGeneralLog(const string& mensaje) {
+    ofstream bitacora;
+
+    bitacora.open("C:/Users/natal/Desktop/u/2024/segundo semestre/estructuras de datos/archivo.txt", ios::app);
+
+    if (bitacora.is_open()) {
+        bitacora << mensaje << endl;
+        bitacora.close();
+    } else {
+        cout << "No se pudo abrir el archivo de bitácora." << endl;
+    }
+}
+void writeToGarbageCollectorLog(const string& mensaje) {
+    ofstream bitacora;
+
+    bitacora.open("C:/Users/natal/Desktop/u/2024/segundo semestre/estructuras de datos/bitacoraBasurero.txt", ios::app);
+
+    if (bitacora.is_open()) {
+        bitacora << mensaje << endl;
+        bitacora.close();
+    } else {
+        cout << "No se pudo abrir el archivo de bitácora." << endl;
+    }
+}
+
 
 
 struct Quality {
@@ -16,19 +87,18 @@ struct Quality {
     Inspector* inspector2;
     GarbageCollector* garbageCollector;
     Storage* storage;
-    // Reference to oven that holds the monsters
-    Monster* first;
+    MonsterQueue* monster_queue;
 
-    Quality(Storage* _storage, GarbageCollector* _garbageCollector) {
+    Quality(Storage* _storage, GarbageCollector* _garbageCollector, MonsterQueue* _monster_queue) {
         inspector1 = new Inspector;
         inspector2 = new Inspector;
-        first = NULL;
+        monster_queue = _monster_queue;
         garbageCollector = _garbageCollector;
         storage = _storage;
     }
 
     bool isEmpty() {
-        if (first == NULL)
+        if (monster_queue->first == NULL)
             return true;
         else
             return false;
@@ -43,10 +113,10 @@ struct Quality {
         if (isEmpty())
             return NULL;
         else {
-            Monster* temp = first;
-            first = first->next;
+            MonsterNode* temp = monster_queue->first;
+            monster_queue->first = monster_queue->first->next;
             temp->next = NULL;
-            return temp;
+            return temp->monster;
         }
     }
 
@@ -56,21 +126,21 @@ struct Quality {
         while (temp != NULL) {
             if (inspector1->activate(temp) == NULL) {
                 garbageCollector->insert(temp);
-                writeToInspector1Log("[" + getTimestamp() + "] The monster " + to_string(temp->id) + " of type " + temp->name + " has been discarded.");
-                writeToGarbageCollectorLog("[" + getTimestamp() + "] The monster " + to_string(temp->id) + " of type " + temp->name + " entered because inspector 1 discarded it.");
-                writeToGeneralLog("[" + getTimestamp() + "] The monster " + to_string(temp->id) + " of type " + temp->name + " entered the garbage collector because inspector 1 discarded it.");
+                writeToInspector1Log("[" + getTimestamp() + "] The monster " +temp->tostring()+ " has been discarded.");
+                writeToGarbageCollectorLog("[" + getTimestamp() + "] The monster " +temp->tostring()+ " entered because inspector 1 discarded it.");
+                writeToGeneralLog("[" + getTimestamp() + "] The monster " +temp->tostring()+ " entered the garbage collector because inspector 1 discarded it.");
                 temp = dequeue();
             } else {
                 if (inspector2->activate(temp) == NULL) {
                     garbageCollector->insert(temp);
-                    writeToInspector2Log("[" + getTimestamp() + "] The monster " + to_string(temp->id) + " of type " + temp->name + " has been discarded.");
-                    writeToGarbageCollectorLog("[" + getTimestamp() + "] The monster " + to_string(temp->id) + " of type " + temp->name + " entered because inspector 2 discarded it."); // TODO: write it correctly
-                    writeToGeneralLog("[" + getTimestamp() + "] The monster " + to_string(temp->id) + " of type " + temp->name + " entered the garbage collector because inspector 2 discarded it.");
+                    writeToInspector2Log("[" + getTimestamp() + "] The monster " +temp->tostring()+ " has been discarded.");
+                    writeToGarbageCollectorLog("[" + getTimestamp() + "] The monster " +temp->tostring()+" entered because inspector 2 discarded it."); // TODO: write it correctly
+                    writeToGeneralLog("[" + getTimestamp() + "] The monster " +temp->tostring()+ " entered the garbage collector because inspector 2 discarded it.");
                     temp = dequeue();
                 } else {
                     storage->insert(temp);
-                    writeToStorageLog("The monster " + to_string(temp->id) + " of type " + temp->name + " entered at " + getTimestamp());
-                    writeToGeneralLog("[" + getTimestamp() + "] The monster " + to_string(temp->id) + " of type " + temp->name + " entered storage and is available for delivery.");
+                    writeToStorageLog("[" + getTimestamp() + "] The monster " +temp->tostring()+ " entered storage and is available for delivery.");
+                    writeToGeneralLog("[" + getTimestamp() + "] The monster " +temp->tostring()+ " entered storage and is available for delivery.");
                     temp = dequeue();
                 }
             }
